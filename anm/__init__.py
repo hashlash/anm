7e7580f05a474461614aa89b10efc4b863945f33
hashlash <23237214+hashlash@users.noreply.github.com>
2023-01-05 02:46:31 +0000

    Send message to backend

--- anm/__init__.py
+++ anm/__init__.py
@@ -7,5 +7,5 @@ from anm.messages.views import send_message
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
-    app.add_url_rule('/', view_func=send_message)
+    app.add_url_rule('/', view_func=send_message, methods=['GET', 'POST'])
     return app
--- anm/messages/views.py
+++ anm/messages/views.py
@@ -1,5 +1,7 @@
-from flask import render_template
+from flask import render_template, request
 
 
 def send_message():
+    from pprint import pprint
+    pprint(request.form)
     return render_template('messages/send.html')
