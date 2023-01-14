94590b72a254ea8535239a77c5db1547fa7a772e
hashlash <23237214+hashlash@users.noreply.github.com>
2023-01-14 06:41:40 +0000

    Allow specify avatar using env var

--- anm/__init__.py
+++ anm/__init__.py
@@ -9,10 +9,11 @@ from anm.messages.views import send_message
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv('DATABASE_URL', 'sqlite:///project.db')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///project.db')
     db.init_app(app)
     migrate.init_app(app, db)
 
+    app.config['AVATAR_URL'] = os.getenv('AVATAR_URL', 'https://picsum.photos/200')
     app.add_url_rule('/', view_func=send_message, methods=['GET', 'POST'])
 
     return app
